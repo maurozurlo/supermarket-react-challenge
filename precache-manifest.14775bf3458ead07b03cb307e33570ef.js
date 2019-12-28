6ec8fa916da9987c1ff422ad3210ef21287bd49e
Mauro Zurlo <mauro.e.zurlo@gmail.com>
2019-12-28 19:17:48 -0300

    Updates

---
+++ precache-manifest.14775bf3458ead07b03cb307e33570ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb24313344abd5970e42b25107b3492",
+    "url": "/supermarket-react-challenge/index.html"
+  },
+  {
+    "revision": "272593d60260e97da228",
+    "url": "/supermarket-react-challenge/static/css/main.3fcdf6de.chunk.css"
+  },
+  {
+    "revision": "d97886313773f71994d5",
+    "url": "/supermarket-react-challenge/static/js/2.23050c07.chunk.js"
+  },
+  {
+    "revision": "8ab7fe2fd1e80d60db4be328fb380e5b",
+    "url": "/supermarket-react-challenge/static/js/2.23050c07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "272593d60260e97da228",
+    "url": "/supermarket-react-challenge/static/js/main.d715b02f.chunk.js"
+  },
+  {
+    "revision": "69d1663558894113208d",
+    "url": "/supermarket-react-challenge/static/js/runtime-main.c666108a.js"
+  },
+  {
+    "revision": "9b217cd28344746e1e639ef5d24a5278",
+    "url": "/supermarket-react-challenge/static/media/delete-icon.9b217cd2.svg"
+  }
+]);
